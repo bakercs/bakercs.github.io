a5330d512923c3d89e3b514e22be64aae13610cf
1egoman <rsg1egoman@gmail.com>
2015-03-19 17:20:09 -0400

    added resources for index page

---
+++ src/js/header-footer.js
@@ -0,0 +1,33 @@
+// header-footer.js
+// This is a simple dependancy that adds in the header and footer in their
+// respective locations in the DOM.
+// (`<header>`, and `<footer>`, respectively)
+$(document).ready(function() {
+
+  // get current location
+  currentLocation = location.pathname.split("/").slice(1).join("/")+"/";
+  if (currentLocation.split('')[currentLocation.length-1] == "/") {
+    currentLocation += "index.html";
+  };
+
+  // add partial header, footer, and other partial stuff to the DOM
+  $.each(["header", "footer"], function(ct, name) {
+    $.get("/partials/"+name+".html", function(data) {
+
+      // add the partial code piece to the correct location
+      $(data).appendTo(name);
+
+      // some header specific code
+      // update the navbar header "active" property for the navbar
+      name === "header" &&
+      $("#bakercs-navbar .navbar-nav li").each(function(ct, item) {
+        if ($(item).data("page") === currentLocation) {
+          $(item).addClass("active");
+        };
+      });
+
+
+    });
+  });
+
+});
